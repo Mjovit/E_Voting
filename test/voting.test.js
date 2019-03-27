9a31bc48f487f72e03363b3f964244d5c700421a
Jewel Jo Prince <jeweljoesphprince@gmail.com>
2019-03-27 10:35:38 +0530

    testing

--- test/voting.test.js
+++ test/voting.test.js
@@ -3,44 +3,38 @@ const ganache = require('ganache-cli');
 const Web3 = require('web3');
 const web3 = new Web3(ganache.provider());
 
-const compiledVoting = require('../ethereum/build/Voting.json');
+const { interface, bytecode } = require('../compile');
 
-
-
+let voting;
 let accounts;
-let vote;
-//let campaignAddress;
-//let campaign;
-
 
 beforeEach(async () => {
- accounts = await web3.eth.getAccounts(); 
- 
- vote = await new web3.eth.Contract(JSON.parse(compiledVoting.interface))
-   .deploy({data : compiledVoting.bytecode })
-   .send({ from: accounts[0] , gas: '1000000' });
+  accounts = await web3.eth.getAccounts();
 
+  voting = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode })
+    .send({ from: accounts[0], gas: '20000000' });
 });
 
- describe('E-voting Contract', () => {
-     it('deploys a contract', () => {
-         assert.ok(vote.options.address);
-     });
-     it('marks caller as the manager', async () => {
-        const manager = await vote.methods.manager().call();
-        assert.equal(accounts[0],manager);
+describe('E-voting Contract', () => {
+  it('deploys a contract', () => {
+    assert.ok(voting.options.address);
+  });
+
+  it('allows one account to enter', async () => {
+    await voting.methods.addCandidate('cand1', 'party1').send({
+      from: accounts[0]
     });
-     it('allows a manager to add a candidate', async () => {
-        await vote.methods
-            .addCandidate('Mathew' , 'congress')
-            .send({
-                from:accounts[0],
-                gas: '1000000'
-            });
-        const add = await vote.methods.candidates(0).call();
-        assert.equal('Mathew' , add.name);
+    const result = await voting.methods.candidate.call({
+      from: accounts[0]
+    });
+    assert.equal(result[0], 'cand1');
+    assert.equal(result[1], 'party1');
 
-       
-     });
-
- });
+    const numcandidates = await voting.methods.getNumOfCandidates().call({
+      from: accounts[0]
+    });
+    assert.equal(accounts[0], candidate[0]);
+    assert.equal(1, numcandidates);
+  });
+});
